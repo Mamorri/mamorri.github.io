e8ccc9e5faa1ff9779b14f9cab06ee95a1f3efa9
mamorri <mandm.4819@gmail.com>
2024-07-23 12:02:30 -0700

    link fixes

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+(() => {
+  const buttonRow = document.querySelector('.button-row');
+  const typed = document.querySelector('.typed');
+
+  const additionalStrings = [
+  'Software Engineer',
+   'Q.A. Specialist',
+    'UX Designer',
+    
+  ];
+  const lastLoadedTime = parseInt(localStorage.getItem('lastLoaded'), 10) || 0;
+  const lastLoaded = new Date(lastLoadedTime);
+  const now = new Date();
+  // Only show the additional strings if it has been more than one year
+  // or more than one month since the user has last seen it.
+  const shouldShowStrings = lastLoaded.getYear() < now.getYear()
+      || lastLoaded.getMonth() < now.getMonth();
+
+  new Typed('.typed .text', {
+    backSpeed: 30,
+    onComplete() {
+      buttonRow.classList.remove('hidden');
+      typed.classList.remove('before-buttons');
+      if (shouldShowStrings) {
+        localStorage.setItem('lastLoaded', Date.now());
+      }
+    },
+    showCursor: false,
+    strings: [].concat(
+      shouldShowStrings ? additionalStrings : [],
+      'Michael Morrison',
+    ),
+    typeSpeed: 30,
+  });
+
+  mdc.autoInit();
+})();
